4f7ae192b9c24d7a9aa67da27f79c9bd262e4630
Louis Liu <lusinga@126.com>
2022-10-20 02:59:00 +0800

    增加八门

--- src/qimen/bamen.go
+++ src/qimen/bamen.go
@@ -1,1 +1,21 @@
 package qimen
+
+type BaMen struct {
+	Id uint8
+}
+
+const (
+	XiuMen = iota
+	ShengMen
+	ShangMen
+	DuMen
+	Jing3Men
+	SiMen
+	Jing1Men
+	KaMen
+)
+
+func (pbm *BaMen) GetName() string {
+	names := []string{"休", "生", "伤", "杜", "景", "死", "惊", "开"}
+	return names[pbm.Id%8]
+}
