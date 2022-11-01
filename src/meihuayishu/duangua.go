02d18d0d6720a3e32e5b02328b0f7fe2b18ffe93
Louis Liu <lusinga@126.com>
2022-11-02 01:51:13 +0800

    梅花易数体用断吉凶

--- src/meihuayishu/duangua.go
+++ src/meihuayishu/duangua.go
@@ -1,5 +1,7 @@
 package meihuayishu
 
+// 年月日时起例。年月日为上卦。年月日加时总数为下卦。又以年月日时总数取爻
+
 func ShiJianQiGua(year int, month int, day int, hour int) {
 	var shangGua uint8 = uint8((year + month + day) % 8)
 	//println("上卦：", shangGua)
@@ -13,6 +15,31 @@ func ShiJianQiGua(year int, month int, day int, hour int) {
 	println("变爻：", bianYao)
 	println(hour)
 	println(year+month+day, year+month+day+hour)
+
+	fullGua := NewGua64FromNumbers(int(xiaGua), int(shangGua), bianYao)
+	println("卦名：", fullGua.BenGua.GetName())
+	//fmt.Printf("%b\n", fullGua.Value)
+
+	println("互卦：", fullGua.HuGua.GetName())
+
+	println("变卦：", fullGua.BianGua.GetName())
+
+	fullGua.DuanGua()
+}
+
+// 二字占：二字为两仪平分，以一字为上卦，以一字为下卦
+func LianZiQiGua(zi1 int, zi2 int, hour int) {
+	var shangGua uint8 = uint8(zi1 % 8)
+	//println("上卦：", shangGua)
+	//sg := NewGuaFromNumber(int(shangGua))
+	//println("上卦：", sg.GetName())
+	var xiaGua uint8 = uint8(zi2 % 8)
+	//println("下卦：", xiaGua)
+	//xg := NewGuaFromNumber(int(xiaGua))
+	//println("下卦：", xg.GetName())
+	bianYao := (zi1 + zi2 + hour) % 6
+	println("变爻：", bianYao)
+	println(hour)
 
 	fullGua := NewGua64FromNumbers(int(xiaGua), int(shangGua), bianYao)
 	println("卦名：", fullGua.BenGua.GetName())
--- src/meihuayishu/test.go
+++ src/meihuayishu/test.go
@@ -4,17 +4,6 @@ import "qimen/src/qimen/dizhi"
 
 func Test() {
 
-	for i := 1; i <= 8; i++ {
-		bagua := NewGuaFromNumber(i)
-		println(bagua.GetName())
-	}
-
 	ShiJianQiGua(2, 11, 24, dizhi.Shen+1)
-
-	var gua1 = Gua64{Value: 0}
-	var huGua1 = gua1.GetHuGua()
-	println(gua1.GetName())
-	name1 := huGua1.GetName()
-	println(name1)
-
+	LianZiQiGua(9, 12, 2)
 }
