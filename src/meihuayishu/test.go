3fa457b7361400563d2a3fef50359834d164026c
Louis Liu <lusinga@126.com>
2022-11-02 01:57:03 +0800

    康节先生观梅占

--- src/meihuayishu/test.go
+++ src/meihuayishu/test.go
@@ -4,6 +4,9 @@ import "qimen/src/qimen/dizhi"
 
 func Test() {
 
-	ShiJianQiGua(2, 11, 24, dizhi.Shen+1)
-	LianZiQiGua(9, 12, 2)
+	//ShiJianQiGua(2, 11, 24, dizhi.Shen+1)
+
+	// 观梅占例
+	ShiJianQiGua(dizhi.Chen+1, 12, 17, dizhi.Shen+1)
+	//LianZiQiGua(9, 12, 2)
 }
