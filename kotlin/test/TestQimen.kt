315eae672d82535376c6f8df78c6663452cfc484
Xulun <lusinga@126.com>
2023-07-07 02:26:27 +0800

    update

---
+++ kotlin/test/TestQimen.kt
@@ -0,0 +1,10 @@
+import com.github.lusing.qimen.QiMenEngine
+import org.junit.Test
+
+class TestQimen {
+    @Test
+    fun testJunHao(): Unit {
+        val qimen = QiMenEngine()
+        qimen.run()
+    }
+}
