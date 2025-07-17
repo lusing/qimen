cf53d55d57bc48ad4a727f5a7de41af39af9ab5c
Louis Liu <lusinga@126.com>
2025-07-17 22:18:25 +0800

    计算始击

--- kotlin/src/main/kotlin/com/github/lusing/taiyi/Cell.kt
+++ kotlin/src/main/kotlin/com/github/lusing/taiyi/Cell.kt
@@ -3,22 +3,25 @@ package com.github.lusing.taiyi
 import com.github.lusing.qimen.DiZhi
 
 class Cell {
-    var gongNum : Int = 0
-    var gongOrder : Int = 0
-    var name : String = ""
-    var jianShenName : String = ""
-    var dz : DiZhi? = null
+    var id: Int = 0
+    var gongNum: Int = 0
+    var gongOrder: Int = 0
+    var name: String = ""
+    var jianShenName: String = ""
+    var dz: DiZhi? = null
     val shenList = mutableListOf<XingShen>()
-    constructor(num: Int, name: String,jcName: String,diZhi: DiZhi?=null, gongOrder:Int=0){
+
+    constructor(num: Int, name: String, jcName: String, diZhi: DiZhi? = null, gongOrder: Int = 0, id: Int = 0) {
         this.gongNum = num
         this.name = name
         this.jianShenName = jcName
         this.dz = diZhi
         this.gongOrder = gongOrder
+        this.id = id
     }
 
-    fun printShen(){
-        for(i in shenList){
+    fun printShen() {
+        for (i in shenList) {
             print(i.name)
             print("<br>")
         }
--- kotlin/src/main/kotlin/com/github/lusing/taiyi/TaiyiPan.kt
+++ kotlin/src/main/kotlin/com/github/lusing/taiyi/TaiyiPan.kt
@@ -7,22 +7,22 @@ class TaiyiPan {
     val pan = Array(5) { Array(5) { Cell(0, "", "") } }
 
     init {
-        pan[4][4] = Cell(1, "乾", "阴德", null, 0)
-        pan[4][3] = Cell(8, "亥", "大义", DiZhi(DiZhi.HAI), 1)
-        pan[4][2] = Cell(8, "子", "地主", DiZhi(DiZhi.ZI), 0)
-        pan[4][1] = Cell(3, "丑", "阳德", DiZhi(DiZhi.CHOU), 1)
-        pan[4][0] = Cell(3, "艮", "和德", null, 0)
-        pan[3][0] = Cell(4, "寅", "吕申", DiZhi(DiZhi.YIN), 1)
-        pan[2][0] = Cell(4, "卯", "高丛", DiZhi(DiZhi.MAO), 0)
-        pan[1][0] = Cell(9, "辰", "太阳", DiZhi(DiZhi.CHEN), 1)
-        pan[0][0] = Cell(9, "巽", "大炅", null, 0)
-        pan[0][1] = Cell(2, "巳", "大神", DiZhi(DiZhi.SI), 1)
-        pan[0][2] = Cell(2, "午", "大威", DiZhi(DiZhi.WU), 0)
-        pan[0][3] = Cell(7, "未", "天道", DiZhi(DiZhi.WEI), 1)
-        pan[0][4] = Cell(7, "坤", "大武", null, 0)
-        pan[1][4] = Cell(6, "申", "武德", DiZhi(DiZhi.SHEN))
-        pan[2][4] = Cell(6, "酉", "太簇", DiZhi(DiZhi.YOU))
-        pan[3][4] = Cell(1, "戌", "阴主", DiZhi(DiZhi.XU), 1)
+        pan[4][4] = Cell(1, "乾", "阴德", null, 0, 13)
+        pan[4][3] = Cell(8, "亥", "大义", DiZhi(DiZhi.HAI), 1, 14)
+        pan[4][2] = Cell(8, "子", "地主", DiZhi(DiZhi.ZI), 0, 15)
+        pan[4][1] = Cell(3, "丑", "阳德", DiZhi(DiZhi.CHOU), 1, 16)
+        pan[4][0] = Cell(3, "艮", "和德", null, 0, 1)
+        pan[3][0] = Cell(4, "寅", "吕申", DiZhi(DiZhi.YIN), 1, 2)
+        pan[2][0] = Cell(4, "卯", "高丛", DiZhi(DiZhi.MAO), 0, 3)
+        pan[1][0] = Cell(9, "辰", "太阳", DiZhi(DiZhi.CHEN), 1, 4)
+        pan[0][0] = Cell(9, "巽", "大炅", null, 0, 5)
+        pan[0][1] = Cell(2, "巳", "大神", DiZhi(DiZhi.SI), 1, 6)
+        pan[0][2] = Cell(2, "午", "大威", DiZhi(DiZhi.WU), 0, 7)
+        pan[0][3] = Cell(7, "未", "天道", DiZhi(DiZhi.WEI), 1, 8)
+        pan[0][4] = Cell(7, "坤", "大武", null, 0, 9)
+        pan[1][4] = Cell(6, "申", "武德", DiZhi(DiZhi.SHEN), 1, 10)
+        pan[2][4] = Cell(6, "酉", "太簇", DiZhi(DiZhi.YOU), 0, 11)
+        pan[3][4] = Cell(1, "戌", "阴主", DiZhi(DiZhi.XU), 1, 12)
         var cell17 = pan[2][2]
         cell17.gongNum = 5
     }
@@ -36,7 +36,9 @@ class TaiyiPan {
         // 合神
         luoDiZhi(ty.getHeShen(), XingShen(XingShen.HE_SHEN))
         // 計神
-        luoDiZhi(ty.getJiShen(true), XingShen(XingShen.JI_SHEN))
+        val jishen = luoDiZhi(ty.getJiShen(true), XingShen(XingShen.JI_SHEN))
+        println("計神=${jishen}")
+
         // 文昌
         val wc_cell = luoWenChang(ty.getWenChang(true), XingShen(XingShen.WEN_CHANG))
 
@@ -47,10 +49,20 @@ class TaiyiPan {
         val zhuDaJiang = zhuSuan?.rem(10)
         println("主大將數：${zhuDaJiang}")
         zhuDaJiang?.let { luoGong(it, XingShen(XingShen.ZHU_DAJIANG)) }
+        val wc_start = wc_cell?.id
 
         val zhuCanJiang = zhuDaJiang?.let { (it * 3) % 10 }
         println("主參將數：${zhuCanJiang}")
         zhuCanJiang?.let { luoGong(it, XingShen(XingShen.ZHU_CANJIANG)) }
+
+        //計算始擊
+        // 計算計神到艮和德的距離
+        val stepJi = calcJiShen(jishen)
+        println("計神需要走${stepJi}步")
+        println("始擊起始位置${wc_start}")
+        val wc_stop = wc_start?.plus(stepJi)
+        println("始擊結束位置${wc_stop}")
+        wc_stop?.let { luoId(it, XingShen(XingShen.SHI_JI)) }
     }
 
     fun mapGongToPos(gong: Int): Cell? {
@@ -100,8 +112,14 @@ class TaiyiPan {
         cell?.shenList?.add(shen)
     }
 
+    fun luoId(id: Int, shen: XingShen) {
+        val cell = findCellById(id)
+        cell?.shenList?.add(shen)
+    }
+
     // 根據地支來確定位置
-    fun luoDiZhi(diZhi: DiZhi, shen: XingShen) {
+    fun luoDiZhi(diZhi: DiZhi, shen: XingShen): Int {
+        var id = 0
         for (i in this.pan) {
             for (j in i) {
                 if (j.dz == null) {
@@ -109,10 +127,23 @@ class TaiyiPan {
                 } else {
                     if (j.dz?.diZhi == diZhi.diZhi) {
                         j.shenList.add(shen)
+                        id = j.id
                     }
                 }
             }
         }
+        return id
+    }
+
+    fun findCellById(id: Int): Cell? {
+        for (i in this.pan) {
+            for (j in i) {
+                if (j.id == id) {
+                    return j
+                }
+            }
+        }
+        return null
     }
 
     fun walk() {
@@ -124,6 +155,7 @@ class TaiyiPan {
                     print(" | ")
                     continue
                 }
+                //print(j.id)
                 print(j.name)
                 print("<br>")
                 print(j.jianShenName)
@@ -154,6 +186,10 @@ class TaiyiPan {
                 }
             }
             return dist + order
+        }
+
+        fun calcJiShen(start: Int): Int {
+            return (17 - start) % 16
         }
 
         fun getNext(cur: Int): Int {
